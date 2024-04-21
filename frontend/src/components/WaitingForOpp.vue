1ed1a1a9100881da0bfb641fea4c48ec435dfbc9
mikolajkozlowskiii <mikolajkozlowskiii@gmail.com>
2024-04-21 18:20:57 +0200

    Initial commit

---
+++ frontend/src/components/WaitingForOpp.vue
@@ -0,0 +1,28 @@
+<template>
+  <h3>Waiting...</h3>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+h3 {
+  font-size: 4vh;
+  margin-bottom: 3vh;
+}
+
+img {
+  transform: scale(0.9);
+}
+
+@media screen and (max-device-width: 480px) {
+  img {
+    transform: scale(0.55);
+    position: relative;
+    right: 60px;
+  }
+}
+
+</style>
